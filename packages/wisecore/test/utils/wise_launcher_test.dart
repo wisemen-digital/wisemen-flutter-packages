ce00b60eb464c4030b8a26178ab6679ed6aaeb5f
Danne Dullers <danne.dullers@appwise.be>
2025-03-20 10:26:24 +0100

    CIR3-86 Add utils and launcher tests

---
+++ packages/wisecore/test/utils/wise_launcher_test.dart
@@ -0,0 +1,112 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
+import 'package:wisecore/src/utils/wise_launcher.dart';
+
+
+class MockUrlLauncherPlatform extends Mock with MockPlatformInterfaceMixin implements UrlLauncherPlatform {}
+class FakeLaunchOptions extends Fake implements LaunchOptions {}
+
+void main() {
+  late MockUrlLauncherPlatform mockUrlLauncher;
+
+  setUpAll(() {
+    registerFallbackValue(FakeLaunchOptions());
+  });
+
+  setUp(() {
+    mockUrlLauncher = MockUrlLauncherPlatform();
+    UrlLauncherPlatform.instance = mockUrlLauncher;
+  });
+
+  group('WiseLauncher', () {
+    test('launchUrl calls launchUrl when canLaunchUrl is true', () async {
+      final testUri = Uri.parse('https://example.com');
+      when(() => mockUrlLauncher.canLaunch(any()))
+          .thenAnswer((_) async => true);
+      when(() => mockUrlLauncher.launchUrl(
+        testUri.toString(),
+        any(),
+      )).thenAnswer((_) async => true);
+
+      var callbackCalled = false;
+
+      await WiseLauncher.launchUrl(
+        url: testUri.toString(),
+        onCannotLaunchUrl: () {
+          callbackCalled = true;
+        },
+      );
+
+      expect(callbackCalled, false);
+      verify(() => mockUrlLauncher.canLaunch(testUri.toString())).called(1);
+      verify(() => mockUrlLauncher.launchUrl(
+        testUri.toString(),
+        any(),
+      )).called(1);
+    });
+
+    test('launchUrl calls onCannotLaunchUrl when canLaunchUrl is false', () async {
+      final testUri = Uri.parse('https://example.com');
+      when(() => mockUrlLauncher.canLaunch(any()))
+          .thenAnswer((_) async => false);
+
+      var callbackCalled = false;
+
+      await WiseLauncher.launchUrl(
+        url: testUri.toString(),
+        onCannotLaunchUrl: () {
+          callbackCalled = true;
+        },
+      );
+
+      expect(callbackCalled, true);
+      verify(() => mockUrlLauncher.canLaunch(testUri.toString())).called(1);
+      verifyNever(() => mockUrlLauncher.launchUrl(any(), any()));
+    });
+
+    test('launchEmail calls launchUrl', () async {
+      when(() => mockUrlLauncher.launchUrl(any(), any()))
+          .thenAnswer((_) async => true);
+
+      await WiseLauncher.launchEmail(
+        email: 'test@example.com',
+        subject: 'Test Subject',
+        body: 'Test Body',
+      );
+
+      // Capture the arguments
+      final captured = verify(() => mockUrlLauncher.launchUrl(captureAny(), any()))
+          .captured;
+
+      final uri = captured.first as String;
+
+      expect(uri.startsWith('mailto:test@example.com'), isTrue);
+    });
+
+    test('launchPhone calls launchUrl', () async {
+      when(() => mockUrlLauncher.launchUrl(any(), any()))
+          .thenAnswer((_) async => true);
+
+      await WiseLauncher.launchPhone(phoneNr: '123456');
+
+      verify(() => mockUrlLauncher.launchUrl(
+        'tel:123456',
+        any(),
+      )).called(1);
+    });
+
+    test('launchMap calls launchUrl', () async {
+      when(() => mockUrlLauncher.launchUrl(any(), any()))
+          .thenAnswer((_) async => true);
+
+      await WiseLauncher.launchMap(name: 'Test Place');
+
+      verify(() => mockUrlLauncher.launchUrl(
+        any(that: contains('query=Test+Place')),
+        any(),
+      )).called(1);
+    });
+  });
+}
