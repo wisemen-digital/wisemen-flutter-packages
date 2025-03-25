1c45d05b365bf60f0e8963ac5bb32bff79e42c0f
Danne Dullers <danne.dullers@appwise.be>
2025-03-25 19:05:39 +0100

    CIR3-85 add dropdown model tests

---
+++ packages/wisewidgetslibrary/test/models/dropdown_model_test.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wisewidgetslibrary/src/models/dropdown_model.dart';
+
+class TestDropdownModel implements DropdownModel {
+  @override
+  String getString(BuildContext context) {
+    return 'Test String';
+  }
+
+  @override
+  Widget? get icon => const Icon(Icons.abc);
+}
+
+void main() {
+  testWidgets('DropdownModel getString returns correct string', (WidgetTester tester) async {
+    final model = TestDropdownModel();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) {
+            expect(model.getString(context), 'Test String');
+            return Container();
+          },
+        ),
+      ),
+    );
+  });
+
+  test('DropdownModel icon returns a icon', () {
+    final model = TestDropdownModel();
+    expect(model.icon, isA<Icon>());
+    final icon = model.icon! as Icon;
+    expect(icon.icon, Icons.abc);
+  });
+}
