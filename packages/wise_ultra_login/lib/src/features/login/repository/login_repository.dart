c23c13ff1f20b423b1f0a0dfd9cff2416f685971
Robbe Jansens <robbejansens@MacBook-Pro-van-Robbe.local>
2025-10-21 14:57:41 +0200

    Add token refresh capability

--- packages/wise_ultra_login/lib/src/features/login/repository/login_repository.dart
+++ packages/wise_ultra_login/lib/src/features/login/repository/login_repository.dart
@@ -1,7 +1,10 @@
+import 'package:wiseclient/wiseclient.dart';
+
 import '../login.dart';
 
 abstract interface class LoginRepository {
   List<LoginMethod> getSupportedLoginMethods();
   Future<void> login(LoginMethod method);
   Future<void> logout();
+  Future<OAuth2Token> refresh(OAuth2Token? token);
 }
