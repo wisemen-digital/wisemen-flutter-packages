bc37591b12d3f5e891c14c61c6911f2666d28afa
Cas Lijnen <cas.lijnen@appwise.be>
2026-04-13 11:38:40 +0200

    Changes to extensions and animatednumbers
    - Added opacityToAlpha double extension
    - Added changeOpacity color extension
    - Added darken color extension
    - Added lighten color extension
    - Removed animated_number.dart since its obsolete compared to TweenAnimationBuilder
    - Improved animatedNumbers and animatedNumbersMapped to use TweenAnimationBuilder, also added some extra documentation

--- packages/wisecore/lib/src/extensions/double_extensions.dart
+++ packages/wisecore/lib/src/extensions/double_extensions.dart
@@ -6,4 +6,11 @@ extension DoubleExtensions on double {
   double toRadians() {
     return this * math.pi / 180;
   }
+
+  /// Converts a [double] between 0 and 1 to an alpha value between 0 and 255
+  int opacityToAlpha() {
+    if (this < 0) return 0;
+    if (this > 1) return 255;
+    return (this * 255).round();
+  }
 }
