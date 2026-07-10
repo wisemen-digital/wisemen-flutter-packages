a075e613ba4dfbe02caa881ab7e84e174e4d8bef
danne <danne.dullers@appwise.be>
2026-07-10 15:16:39 +0200

    chore: scaffold wise_feedback package

---
+++ packages/wise_feedback/lib/wise_feedback.dart
@@ -0,0 +1,4 @@
+/// In-app bug reporting for Flutter that files reports as Linear issues.
+library wise_feedback;
+
+// Exports are added by later tasks.
---
+++ packages/wise_feedback/test/scaffold_test.dart
@@ -0,0 +1,8 @@
+// test/scaffold_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('package scaffold loads', () {
+    expect(1 + 1, 2);
+  });
+}
